ddf71f89eeb3b4c213d7f617d9e9c435cbcadf79
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-14 14:29:44 -0700

    Add author_names method to Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,8 @@ class Book < ApplicationRecord
   validates_presence_of :title, :publication_year, :pages
   has_many :author_books
   has_many :authors, through: :author_books
+
+  def author_names
+    authors.pluck(:name)
+  end
 end
