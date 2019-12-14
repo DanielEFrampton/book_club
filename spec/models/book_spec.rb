f51f9e11a1dc581a0ead78ec1445508a1dc43db1
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-14 14:49:25 -0700

    Add unit test for model method author_count on Book

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -22,5 +22,20 @@ RSpec.describe Book, type: :model do
 
       expect(book.author_names).to eq(["Robert Jordan", "Brandon Sanderson"])
     end
+
+    it "can return number of authors" do
+      author_1 = Author.create!(name: "Robert Jordan")
+      author_2 = Author.create!(name: "Brandon Sanderson")
+      book_1 = Book.create!(title: "A Memory of Light",
+                          publication_year: 2013,
+                          pages: 909)
+      book_1.authors << [author_1, author_2]
+      book_2 = author_1.books.create!(title: "The Eye of the World",
+                          publication_year: 1990,
+                          pages: 685)
+
+      expected(book_1.author_count).to eq(2)
+      expected(book_2.author_count).to eq(1)
+    end
   end
 end
