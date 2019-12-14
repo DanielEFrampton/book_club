642d8af8b970002e0399b9bc414e74b0518cdb8e
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-14 14:38:10 -0700

    Refactor feature test to cover multiple-author case

--- spec/features/books/index_spec.rb
+++ spec/features/books/index_spec.rb
@@ -16,6 +16,12 @@ RSpec.describe 'As a user', type: :feature do
     @book_4 = @seuss.books.create!( title: "Bartholomew and the Oobleck",
                                     publication_year: 1949,
                                     pages: 48)
+    @author_3 = Author.create!(name: "Robert Jordan")
+    @author_4 = Author.create!(name: "Brandon Sanderson")
+    @book_5 = Book.create!(title: "A Memory of Light",
+                        publication_year: 2013,
+                        pages: 909)
+    @book_5.authors << [@author_3, @author_4]
   end
 
   describe "When I visit '/books'" do
@@ -23,31 +29,38 @@ RSpec.describe 'As a user', type: :feature do
       visit '/books'
 
       within "#book-#{@book_1.id}" do
-        expect(page).to have_content(@book_1.title)
-        expect(page).to have_content(@book_1.pages)
-        expect(page).to have_content(@book_1.publication_year)
-        expect(page).to have_content(@book_1.author.name)
+        expect(page).to have_content("Title: #{@book_1.title}")
+        expect(page).to have_content("Pages: #{@book_1.pages}")
+        expect(page).to have_content("Publication Year: #{@book_1.publication_year}")
+        expect(page).to have_content("Author(s): #{@sendak.name}")
       end
-      
+
       within "#book-#{@book_2.id}" do
-        expect(page).to have_content(@book_2.title)
-        expect(page).to have_content(@book_2.pages)
-        expect(page).to have_content(@book_2.publication_year)
-        expect(page).to have_content(@book_2.author.name)
+        expect(page).to have_content("Title: #{@book_2.title}")
+        expect(page).to have_content("Pages: #{@book_2.pages}")
+        expect(page).to have_content("Publication Year: #{@book_2.publication_year}")
+        expect(page).to have_content("Author(s): #{@sendak.name}")
       end
 
       within "#book-#{@book_3.id}" do
-        expect(page).to have_content(@book_3.title)
-        expect(page).to have_content(@book_3.pages)
-        expect(page).to have_content(@book_3.publication_year)
-        expect(page).to have_content(@book_3.author.name)
+        expect(page).to have_content("Title: #{@book_3.title}")
+        expect(page).to have_content("Pages: #{@book_3.pages}")
+        expect(page).to have_content("Publication Year: #{@book_3.publication_year}")
+        expect(page).to have_content("Author(s): #{@seuss.name}")
       end
 
       within "#book-#{@book_4.id}" do
-        expect(page).to have_content(@book_4.title)
-        expect(page).to have_content(@book_4.pages)
-        expect(page).to have_content(@book_4.publication_year)
-        expect(page).to have_content(@book_4.author.name)
+        expect(page).to have_content("Title: #{@book_4.title}")
+        expect(page).to have_content("Pages: #{@book_4.pages}")
+        expect(page).to have_content("Publication Year: #{@book_4.publication_year}")
+        expect(page).to have_content("Author(s): #{@seuss.name}")
+      end
+
+      within "#book-#{@book_5.id}" do
+        expect(page).to have_content("Title: #{@book_5.title}")
+        expect(page).to have_content("Pages: #{@book_5.pages}")
+        expect(page).to have_content("Publication Year: #{@book_5.publication_year}")
+        expect(page).to have_content("Author(s): #{@author_3.name}, #{@author_4.name}")
       end
     end
   end
