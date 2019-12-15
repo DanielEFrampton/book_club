1f8966ec7573b90463dfd862fdf005f873510bb7
Daniel Frampton <daniel.e.frampton@gmail.com>
2019-12-14 17:00:12 -0700

    Add unit test for average_pages method on Author model

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -9,6 +9,22 @@ RSpec.describe Author, type: :model do
     it {should have_many(:books).through(:author_books)}
   end
 
-  describe 'methods' do
+  describe 'class methods' do
+    describe 'author_pages' do
+      it "can return average number of pages of all author's books" do
+        jordan = Author.create!(name: "Robert Jordan")
+        sanderson = Author.create!(name: "Brandon Sanderson")
+        book_1 = Book.create!(title: "A Memory of Light",
+                            publication_year: 2013,
+                            pages: 909)
+        book_1.authors << [jordan, sanderson]
+        book_2 = jordan.books.create!(title: "The Eye of the World",
+                            publication_year: 1990,
+                            pages: 685)
+
+        expect(jordan.average_pages).to eq(797)
+        expect(sanderson.average_pages).to eq(909)
+      end
+    end
   end
 end
